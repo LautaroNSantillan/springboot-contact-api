aabe6500835e04c7c62b5cf6a2b7f0f20ace7b67
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-09-16 15:21:56 -0300

    controller done

--- src/main/java/com/ls/contactapi/controller/ContactController.java
+++ src/main/java/com/ls/contactapi/controller/ContactController.java
@@ -3,11 +3,20 @@ package com.ls.contactapi.controller;
 import com.ls.contactapi.domain.Contact;
 import com.ls.contactapi.services.ContactService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static com.ls.contactapi.constant.Constant.PHOTO_DIRECTORY;
+import static org.springframework.util.MimeTypeUtils.IMAGE_JPEG_VALUE;
+import static org.springframework.util.MimeTypeUtils.IMAGE_PNG_VALUE;
 
 @RestController
 @RequestMapping("/contacts")
@@ -24,5 +33,20 @@ public class ContactController {
     public ResponseEntity<Page<Contact>> getContacts(@RequestParam(value="page",defaultValue="0") int page,
                                                      @RequestParam(value="size", defaultValue="10") int size){
         return ResponseEntity.ok().body(contactService.getAllContacts(page, size));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable (value="id") String id){
+        return ResponseEntity.ok().body(contactService.getContact(id));
+    }
+
+    @PutMapping("/photo")
+    public ResponseEntity<String> uploadPhoto(@RequestParam("id")String id, @RequestParam("file")MultipartFile file){
+        return ResponseEntity.ok().body(contactService.uploadPhoto(id,file));
+    }
+
+    @GetMapping(path="/image/{filename}", produces={IMAGE_PNG_VALUE, IMAGE_JPEG_VALUE})
+    public byte[] getPhoto (@PathVariable("filename") String filename) throws IOException{
+        return Files.readAllBytes(Paths.get(PHOTO_DIRECTORY+filename));
     }
 }
