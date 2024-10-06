c7a8efd9f65422bcd61cdc11221a1eb4c19c0223
SantillanLautaroNahuel <santillanlautaronahuel@gmail.com>
2024-10-06 20:05:46 -0300

    save contact

--- front/src/components/Contact.jsx
+++ front/src/components/Contact.jsx
@@ -3,24 +3,47 @@ import { Link } from 'react-router-dom'
 
 const Contact = ({ contact }) => {
     return (
-      <Link to={`/contacts/${contact.id}`} className="contact__item">
-              <div className="contact__header">
-                  <div className="contact__image">
-                      <img src={contact.photoUrl} alt={contact.name}  />
-                  </div>
-                  <div className="contact__details">
-                      <p className="contact_name">{contact.name.substring(0, 15)} </p>
-                      <p className="contact_title">{contact.title}</p>
-                  </div>
-              </div>
-              <div className="contact__body">
-                  <p><i className="bi bi-envelope"></i> {contact.email.substring(0, 20)} </p>
-                  <p><i className="bi bi-geo"></i> {contact.address}</p>
-                  <p><i className="bi bi-telephone"></i> {contact.phone}</p>
-                  <p>{contact.status === 'Active' ? <i className='bi bi-check-circle'></i> : 
-                      <i className='bi bi-x-circle'></i>} {contact.status}</p>
-              </div>
-          </Link>
+        <Link to={`/contacts/${contact.id}`} className="contact__item">
+        <div className="contact__header">
+          <div className="contact__image">
+            <img 
+              src={contact.photoUrl ? contact.photoUrl : 'default-image-url.jpg'} 
+              alt={contact.name ? contact.name : "No Name"} 
+            />
+          </div>
+          <div className="contact__details">
+            <p className="contact_name">
+              {contact.name ? contact.name.substring(0, 15) : "No Name"} 
+            </p>
+            <p className="contact_title">
+              {contact.title ? contact.title : "No Title"}
+            </p>
+          </div>
+        </div>
+        <div className="contact__body">
+          <p>
+            <i className="bi bi-envelope"></i> 
+            {contact.email ? contact.email.substring(0, 20) : "No Email"}
+          </p>
+          <p>
+            <i className="bi bi-geo"></i> 
+            {contact.address ? contact.address : "No Address"}
+          </p>
+          <p>
+            <i className="bi bi-telephone"></i> 
+            {contact.phone ? contact.phone : "No Phone"}
+          </p>
+          <p>
+            {contact.status === 'Active' ? (
+              <i className='bi bi-check-circle'></i>
+            ) : (
+              <i className='bi bi-x-circle'></i>
+            )} 
+            {contact.status ? contact.status : "Unknown Status"}
+          </p>
+        </div>
+      </Link>
+      
     )
   }
 
